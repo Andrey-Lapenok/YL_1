9a209b61839753738bcda57db9fc3174264c92af
AndreyLapenok <84237230+AndreyLapenok@users.noreply.github.com>
2022-12-26 21:46:17 +0300

    Only yellow circle can be

--- main.py
+++ main.py
@@ -5,43 +5,13 @@ from PyQt5.QtGui import QPixmap
 from PIL import Image
 from PIL import Image, ImageDraw
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(520, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.bt = QtWidgets.QPushButton(self.centralwidget)
-        self.bt.setGeometry(QtCore.QRect(160, 10, 200, 30))
-        self.bt.setObjectName("bt")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 50, 500, 500))
-        self.label.setObjectName("label")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 520, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.bt.setText(_translate("MainWindow", "Создать круг"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
-
-
-class Example(QMainWindow, Ui_MainWindow):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
 
         self.w, self.h = 500, 500
         self.image = Image.new("RGBA", (self.w, self.h), (255, 255, 255, 255))
@@ -54,7 +24,7 @@ class Example(QMainWindow, Ui_MainWindow):
 
     def create_circle(self):
         r = random.randint(0, min(self.w, self.h) // 2 + 1)
-        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        color = "yellow"
         self.image = Image.new("RGBA", (self.w, self.h), (255, 255, 255, 255))
         self.draw = ImageDraw.Draw(self.image)
         self.draw.ellipse([(self.w // 2 - r, self.h // 2 - r), (self.w // 2 + r, self.h // 2 + r)],
